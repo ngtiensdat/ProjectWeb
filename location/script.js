5c9ab5930e1dd61fdfcf42ab4e069ddfc848975d
ngtiensdat <datga27@gmail.com>
2024-06-02 15:24:56 +0700

    updateLocation

--- location/script.js
+++ location/script.js
@@ -75,3 +75,19 @@
         });
     });
 </script>
+<script>
+    let lastScrollTop = 0;
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top = "-50px";
+        } else {
+            navbar.style.top = "0";
+        }
+
+        lastScrollTop = scrollTop;
+    });
+</script>
